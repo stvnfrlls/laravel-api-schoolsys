3fb58b6921ac98fadae2f7469e720f715850a09a
Steven Frilles <stvnfrlls@gmail.com>
2026-03-05 21:08:50 +0800

    add account management

--- src/app/Models/User.php
+++ src/app/Models/User.php
@@ -21,6 +21,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'is_active',
     ];
 
     /**
@@ -41,6 +42,7 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
+        'is_active' => 'boolean',
     ];
 
     public function roles()
