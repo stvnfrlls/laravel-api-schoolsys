d25662edfad3c4e234d865708be0c3db60f60eba
Steven Frilles <stvnfrlls@gmail.com>
2026-03-08 00:25:02 +0800

    update entrypoint.sh

--- docker/php/entrypoint.sh
+++ docker/php/entrypoint.sh
@@ -12,5 +12,8 @@ echo ">>> Setting permissions..."
 chown -R www-data:www-data /var/www/storage /var/www/bootstrap/cache
 chmod -R 775 /var/www/storage /var/www/bootstrap/cache
 
+echo ">>> Running migrations..."
+php artisan migrate --force
+
 echo ">>> Starting services..."
 exec /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
