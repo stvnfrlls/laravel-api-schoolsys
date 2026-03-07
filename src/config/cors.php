24849934668de7208c27048cfae0dc0e50e50f06
Steven Frilles <stvnfrlls@gmail.com>
2026-03-08 01:33:26 +0800

    update cors.php

--- src/config/cors.php
+++ src/config/cors.php
@@ -19,7 +19,10 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://schoolsys-six.vercel.app/login', 'http://localhost:5173'],
+    'allowed_origins' => [
+        'http://localhost:5173',
+        'https://schoolsys-six.vercel.app',
+    ],
 
     'allowed_origins_patterns' => [],
 
